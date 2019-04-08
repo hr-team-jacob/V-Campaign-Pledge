86b766f292f1169b54fb620538e66cf5a2e7c0d0
Vlad Igolnikov <vladigolnikov@gmail.com>
2019-04-08 17:47:58 -0400

    add helper functions for DB queries

--- data/index.js
+++ data/index.js
@@ -127,3 +127,86 @@ db.serialize(()=>{
       }
     );
 });
+
+getProduct = (productId) => {
+  db.get(`SELECT * FROM products WHERE id = ${productId}`, 
+    (err, row) => {
+      if (err) { 
+        console.log (`Error retrieving product ${productId} from db >> ${err}`);
+      } else {
+        console.log (`Success! Retrieved product ${productId} from db >> ${row}`);
+        return row;
+      }
+    }
+  );
+};
+
+
+getRewards = (productId) => {
+  db.all(`SELECT * FROM rewards WHERE productId = ${productId} ORDER BY minimum`,
+    (err, rows) => {
+      if (err) { 
+        console.log (`Error retrieving rewards for product ${productId} from db >> ${err}`);
+      } else {
+        console.log (`Success! Retrieved the following rewards for product ${productId} from db >> ${rows}`);
+        return rows;
+      }
+    }
+  );
+};
+
+getPledgeCountForProduct = (productId) => {
+  db.get(`SELECT COUNT(amount) FROM pledges WHERE id = ${productId}`, 
+    (err, row) => {
+      if (err) { 
+        console.log (`Error retrieving pledge count for product ${productId} from db >> ${err}`);
+      } else {
+        console.log (`Success! Retrieved pledge count for product ${productId} from db >> ${row}`);
+        return row;
+      }
+    }
+  );
+};
+
+getPledgeSumForProduct = (productId) => {
+  db.get(`SELECT SUM(amount) FROM pledges WHERE id = ${productId}`, 
+    (err, row) => {
+      if (err) { 
+        console.log (`Error retrieving pledge sum for product ${productId} from db >> ${err}`);
+      } else {
+        console.log (`Success! Retrieved pledge sum for product ${productId} from db >> ${row}`);
+        return row;
+      }
+    }
+  );
+};
+
+
+getPledgeCountForReward = (rewardId) => {
+  db.get(`SELECT COUNT(amount) FROM pledges WHERE rewardId = ${rewardId}`, 
+    (err, row) => {
+      if (err) { 
+        console.log (`Error retrieving pledge count for reward ${rewardId} from db >> ${err}`);
+      } else {
+        console.log (`Success! Retrieved pledge count for reward ${rewardId} from db >> ${row}`);
+        return row;
+      }
+    }
+  );
+};
+
+addPledge = (productId, rewardId = null, amount) => {
+  db.run(
+    'INSERT INTO pledges (productId, rewardId, amount) VALUES (?, ?, ?)',
+    [
+      productId,
+      rewardId,
+      amount
+    ],
+    err => { 
+      console.log (`Error adding pledge of $${amount} to DB >> ${err}`); 
+    }
+  );
+};
+
+module.exports = {getProduct, getRewards, getPledgeCountForProduct, getPledgeSumForProduct, getPledgeCountForReward, addPledge};
