86758eecdc6e72316388ad7798401b809b5b987a
Vlad Igolnikov <vladigolnikov@gmail.com>
2019-04-09 18:16:10 -0400

    start react component and make axios req to server

---
+++ client/src/components/index.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: {},
+      rewards: []
+    };
+
+    this.fetchProduct = this.fetchProduct.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchProduct();
+  }
+
+  fetchProduct() {
+    var path = window.location.pathname;
+    axios
+      .get(`/${path}`)
+      .then(response => {
+        console.log('got this response >>>>>>> ', response);
+        this.setState(JSON.parse(response));
+      })
+      .catch(error => {
+        console.log('error getting product info from API', error);
+      });
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <h2>Hackstarter</h2>
+        <div className='summary'>
+          <h3>Dolla Dolla Bills Yall</h3>
+        </div>
+        <div className='rewards'>
+          <rewardView rewards={this.state.rewards} />
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
