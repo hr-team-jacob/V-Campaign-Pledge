f2dcc52c9a470b7f2b6fd3ac01ba8c85c7dd956d
Vlad Igolnikov <vladigolnikov@gmail.com>
2019-04-10 16:52:11 -0400

    map over rewards to render each

--- client/src/components/rewardView.jsx
+++ client/src/components/rewardView.jsx
@@ -19,7 +19,10 @@ class RewardView extends React.Component {
   handleSubmit(event) {
     var path = window.location.pathname;
     axios
-      .post(`/${path}`, { amount: this.state.inputVal })
+      .post(`/${path}`, {
+        amount: this.state.inputVal,
+        reward: (reward.id = null)
+      })
       .then(response => console.log('Added pledge -->', response.data))
       .catch(error => console.log('Error adding pledge -->', error));
     event.preventDefault();
@@ -27,17 +30,45 @@ class RewardView extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Pledge X Amount of cash money
-          <input
-            type='text'
-            value={this.state.inputVal}
-            onChange={this.handleChange}
-          />
-        </label>
-        <input type='submit' value='Continue' />
-      </form>
+      <div className='reward'>
+        {this.props.rewards.map((reward, index) => {
+          return (
+            <div key={index}>
+              <div className='rewardMin'>
+                <h2>Pledge ${reward.minimum} or more</h2>
+              </div>
+              <div className='rewardTitle'>
+                <h3>{reward.title}</h3>
+              </div>
+              <div className='rewardDesc'>
+                <p>{reward.description}</p>
+              </div>
+              <div className='rewardDeliv'>
+                <h6>ESTIMATED DELIVERY</h6>
+                <h4>{reward.estDelivery}</h4>
+              </div>
+              <div className='shipsTo'>
+                <h6>SHIPS TO</h6>
+                <h4>Anywhere in the world</h4>
+              </div>
+              <div className='numBackers'>
+                <h6>{reward.backers} backers</h6>
+              </div>
+              <form onSubmit={this.handleSubmit}>
+                <label>
+                  Pledge amount
+                  <input
+                    type='text'
+                    value={this.state.inputVal}
+                    onChange={this.handleChange}
+                  />
+                </label>
+                <input type='submit' value='Continue' />
+              </form>
+            </div>
+          );
+        })}
+      </div>
     );
   }
 }
